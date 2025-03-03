37245dc2c08c80eafd4893aa65e5974fa0656280
N.K. Senadeera <naduni.20191206@iit.ac.lk>
2025-03-03 14:50:02 +0000

    simple commit for the js script changes

---
+++ script.js
@@ -0,0 +1,4 @@
+//taskList, AddTaskButton, taskInput
+const taskInput = document.getElementById('taskInput');
+const AddTaskButton = document.getElementById('AddTaskButton');
+const taskList = document.getElementById('taskList');
